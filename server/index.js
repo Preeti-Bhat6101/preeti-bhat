127d099bea2d8d4049c057c417eb9d8032053b7e
Preeti-Bhat6101 <preetivb05@gmail.com>
2026-06-12 15:53:39 +0530

    connect mongodb

--- server/index.js
+++ server/index.js
@@ -1,7 +1,22 @@
 const express = require("express");
-const app = express();
-const PORT = 5000;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error:", err));
+
+// Test route
 app.get("/", (req, res) => {
   res.send("Preeti Bhat API is running!");
 });
