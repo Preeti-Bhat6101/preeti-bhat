c4d4b7fceade39f78dc61679f6073e6390152b37
Preeti-Bhat6101 <preetivb05@gmail.com>
2026-06-20 18:39:37 +0530

    navbar responsiveness

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,37 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const closeMenu = () => setMenuOpen(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        <Link to="/">Preeti Bhat</Link>
+        <Link to="/" onClick={closeMenu}>
+          Preeti Bhat
+        </Link>
       </div>
-      <ul className="navbar-links">
+
+      <button className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      <ul className={`navbar-links ${menuOpen ? "open" : ""}`}>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" onClick={closeMenu}>
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/projects">Projects</Link>
+          <Link to="/projects" onClick={closeMenu}>
+            Projects
+          </Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about" onClick={closeMenu}>
+            About
+          </Link>
         </li>
         <li>
-          <Link to="/achievements">Achievements</Link>
+          <Link to="/achievements" onClick={closeMenu}>
+            Achievements
+          </Link>
         </li>
         <li>
-          <Link to="/resume">Resume</Link>
+          <Link to="/resume" onClick={closeMenu}>
+            Resume
+          </Link>
         </li>
         <li>
-          <Link to="/chronicles">Chronicles</Link>
+          <Link to="/chronicles" onClick={closeMenu}>
+            Chronicles
+          </Link>
         </li>
         <li>
-          <Link to="/gallery">Gallery</Link>
+          <Link to="/gallery" onClick={closeMenu}>
+            Gallery
+          </Link>
         </li>
         <li>
-          <Link to="/contact">Contact</Link>
+          <Link to="/contact" onClick={closeMenu}>
+            Contact
+          </Link>
         </li>
       </ul>
     </nav>
