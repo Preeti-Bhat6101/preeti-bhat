2e27d9ad0c2e7df6b6c2386fcce6d8e58ab7e06c
Preeti-Bhat6101 <preetivb05@gmail.com>
2026-06-20 18:20:10 +0530

    fix eslint warnings

--- client/src/pages/AdminDashboard.js
+++ client/src/pages/AdminDashboard.js
@@ -42,6 +42,7 @@ function AdminDashboard() {
     fetchChronicles();
     fetchPaintings();
     fetchAchievements();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const fetchProjects = async () => {
